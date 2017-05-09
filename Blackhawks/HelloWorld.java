893d5fbbc9c9b94645e9eb614e7e1ad2d0ba225c
CullenStone <cps_stone@yahoo.com>
2017-05-09 15:08:43 -0500

    aklsjeflkasjeklfjasklejfalskjeflkasje

--- Blackhawks/HelloWorld.java
+++ Blackhawks/HelloWorld.java
@@ -1,13 +1,21 @@
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
+import java.util.concurrent.TimeUnit;
 
 public class HelloWorld
 {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
-        //Button.waitForAnyPress();
-        Motor.A.forward();
-        Motor.C.backward();
+
+        Motor.A.setAcceleration(100000);
+        Motor.C.setAcceleration(100000);
+        Motor.A.setSpeed(100000);// 2 RPM
+        Motor.C.setSpeed(100000);// 2 RPM
+        Motor.A.backward();
+        Motor.C.forward();
+        Button.waitForAnyPress();
+        
+        
     }
 }
