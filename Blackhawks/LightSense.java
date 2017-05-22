1266807b854d35ea2243d0c5215f3e823f93524b
jacarolan <jjjkr@att.net>
2017-05-22 13:45:06 -0500

    Stuff Done
    asdf

--- Blackhawks/LightSense.java
+++ Blackhawks/LightSense.java
@@ -1,7 +1,14 @@
 import lejos.nxt.*;
 import lejos.robotics.objectdetection.*;
 public class LightSense implements SensorPortListener {
+    private Robot robot;
+    public LightSense(Robot rob) {
+        robot = rob;
+    }
+
     public void stateChanged(SensorPort a, int b, int c) {
-        System.out.println("Stuff: " + c+" "+b);
+        if(c > BLACK_THRESHOLD) {
+            robot.detectLine();
+        }
     }
 }
--- Blackhawks/Robot.java
+++ Blackhawks/Robot.java
@@ -4,14 +4,17 @@ import lejos.robotics.*;
 public class Robot {
     private static final int DIS_TO_PUSHED_CAN = 10; //The distance to a can when it is being pushed
     private static final int DIR_ERROR = 5; //The degrees the robot can be off of the targeted can by before it makes an adjustment
-    
+    public static final int BLACK_THRESHOLD = 10; //The input value from the light sensor to indicate that it is on black
+
     private DifferentialPilot movePilot;
     private RegulatedMotor rotatePilot;
     private LightSense light;
     private UltrasonicSense distance;
     private double canAngle;
+    private double initCanAngle;
     private double canDis;
-    
+    ScanThread thread;
+
     /**The behavior which the robot is in
      * 0 - Scanning for cans
      * 1 - Turning towards a can
@@ -27,25 +30,39 @@ public class Robot {
         rotatePilot.resetTachoCount();
         canAngle = 180;
         canDis = 0;
+        thread = new ScanThread(rotatePilot);
+        light = new LightSense(this);
+        distance = new UltrasonicSense(this);
+        initAngle = 0;
+        SensorPort.S2.addSensorPortListener(light);
+        SensorPort.S3.addSensorPortListener(distance);
     }
-    
+
     public void main() {
-        
+        while(true) {
+            if(behaviour == 0) {
+                this.getNearestCanAngle;
+                this.switchBehaviour(1);
+            } else if(behaviour == 1) {
+                this.rotateToCan();
+                this.switchBehaaviour(2);
+            } else if(behaviour == 2) {
+                this.moveToCan();
+            } else if(behaviour == 3) {
+                
+            }
+        }
     }
-    
-    /**
-     * Used to interupt current movement to make an adjustment to the direction
-     */
-    public void poke(double offset) {
-        pilot.rotate(offset);
-    }
-    
-    public void moveToCan() {
+
+    public void rotateToCan() {
         movePilot.turn(canAngle);
         rotatePilot.rotateTo(0);
-        
     }
-    
+
+    public void moveToCan() {
+        movePilot.forward();
+    }
+
     /**
      * Scans 360 degrees, then reports the angle to the nearest can and 
      * rotates the ultrasonic back to center. If this is close to 180 degrees then all cans
@@ -55,22 +72,39 @@ public class Robot {
         rotatePilot.rotateTo(0);
         rotatePilot.rotate(360);
         rotatePilot.resetTachoCount();
+        initCanAngle = canAngle;
         return canAngle;
     }
-    
+
     /**
      * Called to change the robots mode
      */
     public void switchBehaviour(int nmode) {
+        thread.terminate();
         if(nmode == 0) {
             rotatePilot.rotateTo(0);
-            rotatePilot.resetTachoCount();
             canAngle = 180;
             canDis = 0;
+        } else if(nmode == 2) {
+            rotatePilot.rotateTo(0);
+            thread.start();
+        } else if(nmode == 3) {
+            movePilot.forward();
         }
         behaviour = nmode;
     }
-    
+
+    /**
+     * Called by light sensor when the robot is on the edge
+     */
+    public void detectEdge() {
+        if(behaviour == 3) {
+            movePilot.stop();
+            movePilot.rotate(initCanAngle-180);
+            this.switchBehaviour(0);
+        }
+    }
+
     /**
      * Called by the Ultrasonic Sensor when a can is detected
      */
@@ -83,7 +117,9 @@ public class Robot {
         } else if(behaviour == 2) {
             canAngle = rotatePilot.getTachoCount();
             canDis = dis;
-            if(Math.abs(canAngle) > DIR_ERROR) {
+            if(canDis < DIS_TO_PUSHED_CAN) {
+                this.switchBehaviour(3);
+            }if(Math.abs(canAngle) > DIR_ERROR) {
                 movePilot.rotate(canAngle);
             }
         }
